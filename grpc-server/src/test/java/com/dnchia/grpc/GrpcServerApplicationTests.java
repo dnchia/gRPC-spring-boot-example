3001784607c33280ba9b642aa3bd5ae4e110eb59
dnchia <contact@dnchia.com>
2023-12-30 17:31:13 +0100

    WIP: Working, but unfinished example of server. Missing the client example & documentation

---
+++ grpc-server/src/test/java/com/dnchia/grpc/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnchia.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
